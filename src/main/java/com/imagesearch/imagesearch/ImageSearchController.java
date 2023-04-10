07ef6a4cb188feca39638dd8e41918873163e35d
Cole P. <pownelcg@mail.uc.edu>
2023-04-10 19:13:14 -0400

    Added layout and basic navigation

--- src/main/java/com/imagesearch/imagesearch/ImageSearchController.java
+++ src/main/java/com/imagesearch/imagesearch/ImageSearchController.java
@@ -2,6 +2,7 @@ package com.imagesearch.imagesearch;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ImageSearchController {
@@ -13,4 +14,41 @@ public class ImageSearchController {
 		
 		return "home";
 	}
+	@RequestMapping(value="/login")
+	public String login()
+	{
+		
+		return "login";
+	}
+	@RequestMapping(value="/signup")
+	public String signup()
+	{
+		
+		return "signup";
+	}
+	@RequestMapping(value="/post")
+	public String post()
+	{
+		
+		return "post";
+	}
+	@RequestMapping(value="/newpost")
+	public String newpost()
+	{
+		
+		return "newpost";
+	}
+	@RequestMapping(value="/profile")
+	public String profile()
+	{
+		
+		return "profile";
+	}
+	@RequestMapping(value="/search")
+	public String search()
+	{
+		
+		return "search";
+	}
+
 }
