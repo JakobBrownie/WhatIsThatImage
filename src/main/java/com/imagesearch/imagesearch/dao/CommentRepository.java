571e35407e421feafc4f28ca79cf089d489572b5
Cole P. <pownelcg@mail.uc.edu>
2023-04-14 18:57:44 -0400

    id query and new layout added

--- src/main/java/com/imagesearch/imagesearch/dao/CommentRepository.java
+++ src/main/java/com/imagesearch/imagesearch/dao/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.imagesearch.imagesearch.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.imagesearch.imagesearch.dto.CommentDTO;
+import com.imagesearch.imagesearch.dto.UserDTO;
 
 public interface CommentRepository extends CrudRepository<CommentDTO, Integer> {
 
+	List<CommentDTO> findByPostId(int postId);
 }
--- src/main/java/com/imagesearch/imagesearch/dao/PostRepository.java
+++ src/main/java/com/imagesearch/imagesearch/dao/PostRepository.java
@@ -1,9 +1,12 @@
 package com.imagesearch.imagesearch.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import com.imagesearch.imagesearch.dto.UserDTO;
 
 import com.imagesearch.imagesearch.dto.PostDTO;
 
 public interface PostRepository extends CrudRepository<PostDTO, Integer> {
 
+	List<PostDTO> findByUserId(int userId);
 }
